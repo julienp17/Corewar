704af164dec082d3598d4551db25e75b8891e77e
Julien Pause <julien.pause@epitech.eu>
2020-04-21 11:25:05 +0400

    Add help to asm

---
+++ asm/include/asm.h
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2020
+** Corewar
+** File description:
+** Assembler header file
+*/
+
+#ifndef ASM_H_
+    #define ASM_H_
+
+    #define ASM_HELP   \
+    "USAGE\n"                                                                  \
+    "       ./asm file_name[.s]\n"                                             \
+    "\n"                                                                       \
+    "DESCRIPTION\n"                                                            \
+    "       file_name       file in assembly language to be converted into "   \
+    "file_name.cor, an\n"                                                      \
+    "                       executable in the Virtual Machine.\n"
+
+
+#endif
--- asm/src/main.c
+++ asm/src/main.c
@@ -2,10 +2,19 @@
 ** EPITECH PROJECT, 2020
 ** CPE_corewar_2019
 ** File description:
-** main
+** Main file for assembler
 */
 
-int main(void)
+#include <stdlib.h>
+#include "my.h"
+#include "asm.h"
+
+int main(int ac, char **av)
 {
+    if (ac == 2 && my_strcmp(av[1], "-h") == 0) {
+        my_putstr(ASM_HELP);
+        return (EXIT_SUCCESS);
+    }
+    
     return (0);
 }
