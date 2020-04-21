a5b18ee52c98fc667180506c3bf9e14a4e589a82
Julien Pause <julien.pause@epitech.eu>
2020-04-21 23:05:08 +0400

    Update asm header file

--- asm/include/asm.h
+++ asm/include/asm.h
@@ -8,6 +8,14 @@
 #ifndef ASM_H_
     #define ASM_H_
 
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <stdint.h>
+    #include <sys/types.h>
+    #include <unistd.h>
+    #include <fcntl.h>
+    #include "op.h"
+
     #define ASM_HELP   \
     "USAGE\n"                                                                  \
     "       ./asm file_name[.s]\n"                                             \
@@ -17,5 +25,29 @@
     "file_name.cor, an\n"                                                      \
     "                       executable in the Virtual Machine.\n"
 
+    #define ASM_EXT         ".s"
+    #define COR_EXT         ".cor"
+    #define REGISTER_CHAR   'r'
+    #define REG_CB          0b01
+    #define DIR_CB          0b10
+    #define IND_CB          0b11
 
+    typedef union param_value {
+        int8_t reg;
+        int16_t indirect;
+        int32_t direct;
+    } param_value_t;
+
+    typedef unsigned int uint;
+
+    int assemble(char const *filename);
+    int encode_instruction(int fd, char const *instruction);
+    char *get_output_filename(char const *filename);
+
+    char **parse_instructions_from_file(char const *filename);
+    char **parse_instruction(char const *instruction);
+
+    op_t get_op_by_name(char const *name);
+    int16_t swap_int16(int16_t val);
+    int32_t swap_int32(int32_t val);
 #endif
