3ad445613124aca73518f978bba17e5848500a5d
Julien Pause <julien.pause@epitech.eu>
2020-04-21 11:27:50 +0400

    Add error handling for asm arguments

--- asm/src/main.c
+++ asm/src/main.c
@@ -11,10 +11,14 @@
 
 int main(int ac, char **av)
 {
-    if (ac == 2 && my_strcmp(av[1], "-h") == 0) {
+    if (ac != 2) {
+        my_puterr("asm: Incorrect number of arguments.\n");
+        my_putstr(ASM_HELP);
+        return (EXIT_FAILURE);
+    }
+    if (my_strcmp(av[1], "-h") == 0) {
         my_putstr(ASM_HELP);
         return (EXIT_SUCCESS);
     }
-    
     return (0);
 }
