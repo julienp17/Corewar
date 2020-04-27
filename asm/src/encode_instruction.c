b85fdd1184f140f85971f3c30cefa24d47cb10f3
Julien Pause <julien.pause@epitech.eu>
2020-04-27 17:42:47 +0400

    Refactor encode_instruction.c

--- asm/src/encode_instruction.c
+++ asm/src/encode_instruction.c
@@ -5,12 +5,14 @@
 ** Encode an instruction
 */
 
+#include <stdbool.h>
 #include "asm.h"
 #include "my.h"
 
 static int write_instruction(int fd, op_t op, char **tokens);
-static ssize_t write_param_value(int fd, char *arg);
 static ssize_t write_coding_byte(int fd, char nbr_args, char **args);
+static ssize_t write_param_value(int fd, op_t op, char *arg);
+static bool arg_is_index(op_t op);
 
 int encode_instruction(int fd, char const *instruction)
 {
@@ -21,12 +23,11 @@ int encode_instruction(int fd, char const *instruction)
     tokens = parse_instruction(instruction);
     if (tokens == NULL)
         return (EXIT_FAILURE);
-    op = get_op_by_name(tokens[0]);
-    if (op.mnemonique == 0) {
-        my_puterr(tokens[0]);
-        my_puterr(" : Invalid instruction.\n");
+    if (instruction_is_correct(tokens) == false)
         return (EXIT_FAILURE);
-    }
+    if (get_argument_type(tokens[0]) == T_LAB)
+        my_strarr_rotate(tokens, 0);
+    op = get_op_by_name(tokens[0]);
     status = write_instruction(fd, op, tokens);
     my_free_str_array(tokens);
     return (status);
@@ -38,11 +39,11 @@ static int write_instruction(int fd, op_t op, char **tokens)
         my_puterr("Error writing to file.\n");
         return (EXIT_FAILURE);
     }
-    if ((op.nbr_args != 1 || op.type[0] != T_DIR))
+    if (op.nbr_args != 1 || op.type[0] != T_DIR)
         if (write_coding_byte(fd, op.nbr_args, tokens + 1) < 0)
             return (EXIT_FAILURE);
     for (int i = 1 ; tokens[i] ; i++)
-        if (write_param_value(fd, tokens[i]) < 0)
+        if (write_param_value(fd, op, tokens[i]) < 0)
             return (EXIT_FAILURE);
     return (EXIT_SUCCESS);
 }
@@ -51,14 +52,12 @@ static ssize_t write_coding_byte(int fd, char nbr_args, char **args)
 {
     unsigned char coding_byte = 0;
     ssize_t write_size = 0;
+    int arg_type = 0;
 
     for (int i = 0 ; args[i] ; i++) {
-        if (args[i][0] == REGISTER_CHAR)
-            coding_byte = (coding_byte << 2) + T_REG;
-        else if (args[i][0] == DIRECT_CHAR)
-            coding_byte = (coding_byte << 2) + T_DIR;
-        else if (my_char_is_num(args[i][0]))
-            coding_byte = (coding_byte << 2) + T_IND;
+        arg_type = get_argument_type(args[i]);
+        arg_type -= (arg_type == T_IND) ? 1 : 0;
+        coding_byte = (coding_byte << 2) + arg_type;
     }
     for (int i = nbr_args ; i < MAX_ARGS_NUMBER ; i++)
         coding_byte <<= 2;
@@ -66,22 +65,35 @@ static ssize_t write_coding_byte(int fd, char nbr_args, char **args)
     return (write_size);
 }
 
-static ssize_t write_param_value(int fd, char *arg)
+static ssize_t write_param_value(int fd, op_t op, char *arg)
 {
-    param_value_t param_value;
     ssize_t write_size = 0;
+    int arg_type = 0;
+    int value = 0;
 
-    if (arg[0] == REGISTER_CHAR) {
-        param_value.reg = my_atoi(arg + 1);
-        write_size = write(fd, &(param_value.reg), 1);
-    } else if (my_char_is_num(arg[0])) {
-        param_value.indirect = my_atoi(arg);
-        param_value.indirect = swap_int16(param_value.indirect);
-        write_size = write(fd, &(param_value.indirect), IND_SIZE);
-    } else if (arg[0] == DIRECT_CHAR) {
-        param_value.direct = my_atoi(arg + 1);
-        param_value.direct = swap_int32(param_value.direct);
-        write_size = write(fd, &(param_value.direct), DIR_SIZE);
+    arg_type = get_argument_type(arg);
+    if (arg_type == T_REG) {
+        value = (char)my_atoi(arg + 1);
+        write_size = write(fd, &value, 1);
+    } else if (arg_type == T_IND) {
+        value = (short int)my_atoi(arg);
+        value = (short int)swap_int16(value);
+        write_size = write(fd, &value, IND_SIZE);
+    } else if (arg_type == T_DIR) {
+        if (arg_is_index(op))
+            return (write_param_value(fd, op, arg + 1));
+        value = my_atoi(arg + 1);
+        value = swap_int32(value);
+        write_size = write(fd, &(value), DIR_SIZE);
     }
     return (write_size);
+}
+
+static bool arg_is_index(op_t op)
+{
+    return (
+        my_str_contains(op.comment, "index")
+        || (op.nbr_args == 1 && op.type[0] == T_DIR
+            && my_strcmp(op.mnemonique, "live") != 0)
+    );
 }
