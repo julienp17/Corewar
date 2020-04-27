be3b1f33d49889bd8b9e4aa0eec19ae3ffbdfcb1
Julien Pause <julien.pause@epitech.eu>
2020-04-27 19:11:55 +0400

    Refactor main

--- corewar/src/main.c
+++ corewar/src/main.c
@@ -5,7 +5,32 @@
 ** main
 */
 
-int main(void)
+#include <stdlib.h>
+#include <stdio.h>
+#include "file_read.h"
+#include "op.h"
+
+int main(int ac, char **av)
 {
+    buffer_t buf;
+    int offset = 0;
+    int offsets[] = {
+        sizeof(int), sizeof(char) * PROG_NAME_LENGTH,
+        sizeof(int), sizeof(int), sizeof(char) * COMMENT_LENGTH, sizeof(int),
+        sizeof(char), 0
+    };
+
+    if (ac != 2) {
+        return (1);
+    }
+    if (buffer_fill_from_file(av[1], &buf) == EXIT_FAILURE)
+        return (1);
+    for (int i = 0 ; offsets[i] != 0 ; i++) {
+        if (offsets[i] == sizeof(int))
+            printf("%x\n", *(int *)(buf.buf + offset));
+        else
+            printf("%s\n", (char *)(buf.buf + offset));
+        offset += offsets[i];
+    }
     return (0);
 }
