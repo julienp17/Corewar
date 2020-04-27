21984970d339eda4a427f0287f76fcf916f0a5d3
Julien Pause <julien.pause@epitech.eu>
2020-04-27 17:40:25 +0400

    Move get_output_filename.c to assemble.c

--- asm/src/assemble.c
+++ asm/src/assemble.c
@@ -9,6 +9,8 @@
 #include "file_control.h"
 #include "my.h"
 
+static char *get_output_filename(char const *filename);
+
 int assemble(char const *filename)
 {
     char **instructions = NULL;
@@ -25,4 +27,14 @@ int assemble(char const *filename)
     free(output_filename);
     my_free_str_array(instructions);
     return (status);
+}
+
+static char *get_output_filename(char const *filename)
+{
+    char *output_filename = NULL;
+
+    output_filename = my_strdup(filename);
+    output_filename = get_pretty_filename(output_filename);
+    output_filename = my_strdupcat(output_filename, COR_EXT);
+    return (output_filename);
 }
