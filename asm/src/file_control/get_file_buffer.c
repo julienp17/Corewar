a4634ab4918f9d36c2b553c21f21c37236ecde18
Julien Pause <julien.pause@epitech.eu>
2020-04-29 19:57:37 +0400

    Refactor get_file_buffer and get_file_size error messages with my_eprintf

--- asm/src/file_control/get_file_buffer.c
+++ asm/src/file_control/get_file_buffer.c
@@ -9,30 +9,49 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include "my.h"
 #include "file_control.h"
+#include "my.h"
+
+static int fill_buffer(char const *filepath, ssize_t file_size, char *buffer);
 
 char *get_file_buffer(char const *filepath)
 {
-    int fd = 0;
     char *buffer = NULL;
-    unsigned int file_size = get_file_size(filepath);
+    ssize_t file_size = 0;
 
+    file_size = get_file_size(filepath);
+    if (file_size < 0)
+        return (NULL);
     buffer = malloc(sizeof(char) * (file_size + 1));
     if (buffer == NULL) {
-        my_puterr("Couldn't allocate memory for file buffer.\n");
-        return (NULL);
-    }
-    fd = open(filepath, O_RDONLY);
-    if (fd < 0) {
-        my_puterr("Couldn't open file buffer.\n");
-        return (NULL);
-    }
-    if (read(fd, buffer, file_size) == -1) {
-        my_puterr("Coudln't read file buffer.\n");
+        my_eprintf("%s : couldn't allocate memory for file buffer.\n",filepath);
         return (NULL);
     }
     buffer[file_size] = '\0';
-    close(fd);
+    if (fill_buffer(filepath, file_size, buffer) == EXIT_FAILURE)
+        return (NULL);
     return (buffer);
+}
+
+static int fill_buffer(char const *filepath, ssize_t file_size, char *buffer)
+{
+    int fd = 0;
+    ssize_t read_size = 0;
+    int status = EXIT_SUCCESS;
+
+    fd = open(filepath, O_RDONLY);
+    if (fd < 0) {
+        my_eprintf("%s : couldn't open file.\n", filepath);
+        return (EXIT_FAILURE);
+    }
+    read_size = read(fd, buffer, file_size);
+    if (read_size == -1) {
+        my_eprintf("%s : couldn't read file.\n", filepath);
+        status = EXIT_FAILURE;
+    }
+    if (close(fd) < 0) {
+        my_eprintf("%s : couldn't close file.\n", filepath);
+        status = EXIT_FAILURE;
+    }
+    return (status);
 }
