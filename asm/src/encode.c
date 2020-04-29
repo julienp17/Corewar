9b13daa173ecd8fc2dea306c3853aeb3354aaa44
Julien Pause <julien.pause@epitech.eu>
2020-04-29 12:10:43 +0400

    Refactor encode by counting the total number of bytes written, and writing it by seeking back to program size

--- asm/src/encode.c
+++ asm/src/encode.c
@@ -29,15 +29,28 @@ int encode_to_file(char const *output_filename, char **instructions)
 
 int encode(int fd, char **instructions)
 {
-    int status = EXIT_SUCCESS;
+    ssize_t total_bytes_written = 0;
+    ssize_t bytes_written = 1;
 
-    status = encode_header(fd, instructions);
-    if (status == EXIT_FAILURE)
+    if (encode_header(fd, instructions) == EXIT_FAILURE)
         return (EXIT_FAILURE);
     my_strarr_rotate(instructions, 0);
     if (my_str_beg(instructions[0], COMMENT_CMD_STRING))
         my_strarr_rotate(instructions, 0);
-    for (uint i = 0 ; status == EXIT_SUCCESS && instructions[i] ; i++)
-        status = encode_instruction(fd, instructions[i]);
-    return (status);
+    for (uint i = 0 ; bytes_written > 0 && instructions[i] ; i++) {
+        bytes_written = encode_instruction(fd, instructions[i]);
+        total_bytes_written += bytes_written;
+    }
+    if (bytes_written <= 0)
+        return (EXIT_FAILURE);
+    if (lseek(fd, sizeof(int) * 2 + sizeof(char) * PROG_NAME_LENGTH, SEEK_SET) < 0){
+        my_puterr("Coudln't lseek back to program size.\n");
+        return (EXIT_FAILURE);
+    }
+    total_bytes_written = swap_int32(total_bytes_written);
+    if (write(fd, &(total_bytes_written), sizeof(int)) < 0) {
+        perror("write");
+        return (EXIT_FAILURE);
+    }
+    return (EXIT_SUCCESS);
 }
