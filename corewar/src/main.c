6f5119cb250400b67658176b4e6e08b1553d5c08
franck <franck-valentino.folio@epitech.eu>
2020-04-30 10:05:24 +0400

    read all hexa of .cor

--- corewar/src/main.c
+++ corewar/src/main.c
@@ -5,7 +5,32 @@
 ** main
 */
 
-int main(void)
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "op.h"
+
+void pars_data(char **av)
 {
+    int fd = 0;
+    void *buffer = NULL;
+
+    buffer = malloc(sizeof(int));
+    fd = open(av[1], O_RDONLY);
+
+    while (read(fd, buffer, sizeof(int))) {
+        printf("%x ", *(int *)buffer);
+    }
+}
+
+int main(int ac, char **av)
+{
+    if (ac < 2)
+        return (84);
+
+    pars_data(av);
     return (0);
 }
