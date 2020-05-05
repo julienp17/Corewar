acf4c6b9063ff7d689905a0d0ed1f57872794f79
Julien Pause <julien.pause@epitech.eu>
2020-05-05 22:30:58 +0400

    Add more checks for program comment

--- asm/src/structures/asm/asm_fill_comment.c
+++ asm/src/structures/asm/asm_fill_comment.c
@@ -8,14 +8,14 @@
 #include "asm.h"
 #include "my.h"
 
+static bool asm_state_is_good(asm_t *asb);
+
 int asm_fill_comment(asm_t *asb, char const *line)
 {
     char **tokens = NULL;
 
-    if (my_str_is_empty(asb->header.comment) == false) {
-        asm_puterr(asb, "The comment can only be defined once");
+    if (asm_state_is_good(asb) == false)
         return (EXIT_FAILURE);
-    }
     tokens = my_str_to_word_array(line, '"');
     my_strarr_remove_empty(tokens);
     if (my_strarr_len(tokens) != 2) {
@@ -23,10 +23,23 @@ int asm_fill_comment(asm_t *asb, char const *line)
         return (EXIT_FAILURE);
     }
     if (my_strlen(tokens[1]) > COMMENT_LENGTH) {
-        asm_puterr(asb, "The program name is too long");
+        asm_puterr(asb, "The comment is too long");
         return (EXIT_FAILURE);
     }
     my_strcpy(asb->header.comment, tokens[1]);
     my_free_str_array(tokens);
     return (EXIT_SUCCESS);
+}
+
+static bool asm_state_is_good(asm_t *asb)
+{
+    if (my_str_is_empty(asb->header.prog_name) || asm_has_an_instruction(asb)) {
+        asm_puterr(asb, "The comment must be just after the name");
+        return (false);
+    }
+    if (my_str_is_empty(asb->header.comment) == false) {
+        asm_puterr(asb, "The comment can only be defined once");
+        return (false);
+    }
+    return (true);
 }
