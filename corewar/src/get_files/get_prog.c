f60dc95afc867538ab6474c0416982dd94291482
tristan bourgeois <tristan@laptop-q0eotgt3-1.home>
2020-05-06 16:33:52 +0400

    files reading

--- corewar/src/get_files/get_prog.c
+++ corewar/src/get_files/get_prog.c
@@ -6,4 +6,19 @@
 */
 
 #include "file_informations.h"
+#include <unistd.h>
 
+void get_prog(fd)
+{
+    int read_size = sizeof(int) +
+                    (sizeof(char) * PROG_NAME_LENGTH) +
+                    (sizeof(int)) +
+                    (sizeof(char) * COMMENT_LENGTH);
+    char buf[read_size];
+
+    read(fd, &buf, read_size);
+    read_size = sizeof(char);
+    for (int status = 0; status > 0; status = read(fd, &buf, read_size)) {
+        
+    }
+}
