b61a565788add0560cf7cb2d0514f261c4c8220f
Julien Pause <julien.pause@epitech.eu>
2020-05-06 17:35:42 +0400

    Add hexdump_diff.sh tester script

---
+++ asm/hexdump_diff.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/sh
+
+output="`basename -s .s $1`.cor"
+my_asm="./asm"
+my_hexdump="my_hexdump"
+reference_asm="./reference/reference_asm"
+reference_hexdump="reference_hexdump"
+
+make > /dev/null && $my_asm $1 && hexdump -C $output > $my_hexdump
+$reference_asm $1 && hexdump -C $output > $reference_hexdump
+
+echo "Testing with ${output}..."
+diff $my_hexdump $reference_hexdump > /dev/null
+if [ $? -eq 0 ]; then
+    echo "Hexdumps are identical"
+else
+    diff -y $my_hexdump $reference_hexdump
+fi
+rm $my_hexdump $reference_hexdump $output
