5e10d8f7f9cb4f3ccb0c381eaffa7b6684080313
Julien Pause <julien.pause@epitech.eu>
2020-05-06 17:54:08 +0400

    Refactor argument_get_type and make it more script

--- asm/src/arguments/argument_get_type.c
+++ asm/src/arguments/argument_get_type.c
@@ -8,15 +8,46 @@
 #include "asm.h"
 #include "my.h"
 
+static bool is_register(char const *arg);
+static bool is_direct(char const *arg);
+static bool is_indirect(char const *arg);
+static bool is_label(char const *arg);
+
 int argument_get_type(char const *arg)
 {
-    if (arg[0] == REGISTER_CHAR)
+    if (is_register(arg))
         return (T_REG);
-    if (arg[0] == DIRECT_CHAR)
+    if (is_direct(arg))
         return (T_DIR);
-    if (arg[0] == LABEL_CHAR)
-        return (T_LAB);
-    if (my_char_is_digit(arg[0]))
+    if (is_indirect(arg))
         return (T_IND);
+    if (is_label(arg))
+        return (T_LAB);
     return (0);
+}
+
+static bool is_register(char const *arg)
+{
+    return (
+        arg[0] == REGISTER_CHAR
+        && my_str_isnum_pos(arg + 1)
+    );
+}
+
+static bool is_direct(char const *arg)
+{
+    return (
+        arg[0] == DIRECT_CHAR
+        && (arg[1] == LABEL_CHAR || my_str_isnum(arg + 1))
+    );
+}
+
+static bool is_indirect(char const *arg)
+{
+    return (my_str_isnum(arg));
+}
+
+static bool is_label(char const *arg)
+{
+    return (arg[0] == LABEL_CHAR);
 }
