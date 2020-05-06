b3af6563dd50a2572c0f4f59f5610b5a45082cc7
franck <franck-valentino.folio@epitech.eu>
2020-05-06 08:17:54 +0400

    segfault get opt fixed

--- corewar/src/get_opt/my_get_opt.c
+++ corewar/src/get_opt/my_get_opt.c
@@ -12,11 +12,15 @@
 
 void elem_add(prog_info_t **add, char **av, int i)
 {
-    (*add)->prog_name = my_strdup(av[i]);   
+    (*add)->prog_name = my_strdup(av[i]);
 
-    if (av[i + 2] != NULL && my_strcmp(av[i + 1], "-a") == 0) {
-        (*add)->load_address = my_atoi(av[i + 2]);
+    if (av[i + 1] != NULL && my_strcmp(av[i + 1], "-a") == 0) {
+        if (av[i + 2] != NULL)
+            (*add)->load_address = my_atoi(av[i + 2]);
     }
+    else
+        (*add)->load_address = -1;
+
 }
 
 void add_prog_infos(char **av, int i , my_get_opt_t *opt)
@@ -27,8 +31,10 @@ void add_prog_infos(char **av, int i , my_get_opt_t *opt)
     add = malloc(sizeof(prog_info_t));
     tmp = opt->prog;
     elem_add(&add, av, i);
-    if (tmp == NULL)
+    if (tmp == NULL) {
         opt->prog = add;
+        return;
+    }
     while (tmp->next != NULL) {
         tmp = tmp->next;
     }
@@ -41,13 +47,13 @@ my_get_opt_t *my_get_opt(char **av)
     my_get_opt_t *opt = NULL;
 
     opt = malloc(sizeof(my_get_opt_t));
+    opt->prog = NULL;
     for (size_t i = 1; av[i]; i++) {
-        if ((!my_strcmp("-dump", av[i])) && (!my_strcmp("-d", av[i]))) {
+        if ((!my_strcmp("-dump", av[i])) || (!my_strcmp("-d", av[i]))) {
             opt->nb_cycle = my_atoi(av[(i + 1)]);
             i++;
         }
         if ((!my_strcmp("-n", av[i]))) {
-            printf("av %s\n", av[i]);
             i++;
             add_prog_infos(av, i, opt);
         }
