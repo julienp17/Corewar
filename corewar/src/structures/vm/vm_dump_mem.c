aeeef893cce008645ef77ccebf885f9fc0c10051
Julien Pause <julien.pause@epitech.eu>
2020-05-11 15:58:30 +0400

    Add vm_dump_mem.c

---
+++ corewar/src/structures/vm/vm_dump_mem.c
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2020
+** Corewar VM
+** File description:
+** Dump the memory of the vm
+*/
+
+#include <stdio.h>
+#include "virtual_machine.h"
+
+void vm_dump_mem(vm_t *vm)
+{
+    int const offset = 0x20;
+
+    for (int i = 0 ; i < MEM_SIZE ; i += offset) {
+        printf("%-4X : ", i);
+        for (int j = 0 ; j < offset ; j++)
+            printf(" %02X", vm->mem[i + j]);
+        printf("\n");
+    }
+}
