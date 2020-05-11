24e77ad6a32f8479371700bfa0dfe4c0256c0b30
Julien Pause <julien.pause@epitech.eu>
2020-05-11 15:26:55 +0400

    Add corewar function prototype in corewar.h

--- corewar/include/corewar.h
+++ corewar/include/corewar.h
@@ -10,9 +10,12 @@
 
     #include <stdint.h>
     #include "op.h"
+    #include "champion_data.h"
 
     int16_t swap_int16(int16_t val);
     int32_t swap_int32(int32_t val);
     op_t op_get_by_code(unsigned char code);
     bool op_has_coding_byte(op_t op);
+
+    int corewar(champion_data_t *filepaths, int nb_prog);
 #endif
