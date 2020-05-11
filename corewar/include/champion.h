b6bf30eed5948d5de7a4c66d2abc2406cae28a61
Julien Pause <julien.pause@epitech.eu>
2020-05-11 15:26:40 +0400

    Add champion.h

---
+++ corewar/include/champion.h
@@ -0,0 +1,32 @@
+/*
+** EPITECH PROJECT, 2020
+** Corewar VM
+** File description:
+** Header file for champion structure
+*/
+
+#ifndef CHAMPION_H_
+    #define CHAMPION_H_
+
+    #include <stdbool.h>
+    #include "op.h"
+    #include "instruction.h"
+
+    #define HEADER_SIZE         (sizeof(int))                           \
+                                + (sizeof(char) * PROG_NAME_LENGTH)     \
+                                + (sizeof(int))                         \
+                                + (sizeof(char) * COMMENT_LENGTH)       \
+
+    typedef struct champion {
+        header_t header;
+        int nb;
+        bool is_alive;
+        int cycle_wait;
+        int registers[REG_NUMBER];
+        int pc;
+        instruction_t *instruction;
+        bool carry;
+    } champion_t;
+
+    int champion_load_header(champion_t *champion, int fd);
+#endif
