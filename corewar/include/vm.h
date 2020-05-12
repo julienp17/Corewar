70eb19472787f6fd867e728888f364ca658c2f7b
Julien Pause <julien.pause@epitech.eu>
2020-05-12 18:42:09 +0400

    Add champion_execute and champion_load_instruction prototypes in vm.h

--- corewar/include/vm.h
+++ corewar/include/vm.h
@@ -18,14 +18,18 @@
 
     typedef unsigned char uchar;
     typedef struct virtual_machine {
-        uchar nb_champions;
+        int cycle;
         char mem[MEM_SIZE];
         champion_t champions[MAX_CHAMPIONS];
+        uchar nb_champions;
+        uchar nb_alive;
     } vm_t;
 
     vm_t *vm_create(void);
     void vm_destroy(vm_t *vm);
 
     int vm_load_champion(vm_t *vm, char const *filepath, int const champion_nb);
-    void vm_dump_mem(vm_t *vm);
+    void vm_dump(vm_t *vm);
+    int champion_execute(vm_t *vm, champion_t *champion);
+    int champion_load_instruction(vm_t *vm, champion_t *champion);
 #endif
