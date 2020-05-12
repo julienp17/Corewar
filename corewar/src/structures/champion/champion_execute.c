22cc2b6a3cf5015e07564bd9f96756b65c32f5b9
Julien Pause <julien.pause@epitech.eu>
2020-05-12 22:29:42 +0400

    Refactor champion_execute

--- corewar/src/structures/champion/champion_execute.c
+++ corewar/src/structures/champion/champion_execute.c
@@ -14,16 +14,21 @@ int champion_execute(vm_t *vm, champion_t *champion)
     int (*operation)(vm_t *, champion_t *) = NULL;
     int status = 0;
 
-    champion_load_instruction(vm, champion);
+    if (champion->cycle_wait > 0) {
+        champion->cycle_wait--;
+        return (EXIT_SUCCESS);
+    }
     operation = operation_get(champion->instruction->op.code);
     if (operation == NULL) {
         champion->cycle_wait = 1;
-        return (EXIT_FAILURE);
+        status = EXIT_FAILURE;
+    } else {
+        status = operation(vm, champion);
+        if (op_modifies_carry(champion->instruction->op))
+            champion->carry = status;
+        champion->cycle_wait = champion->instruction->op.nbr_cycles;
     }
-    status = operation(vm, champion);
-    if (op_modifies_carry(champion->instruction->op))
-        champion->carry = status;
-    champion->cycle_wait = champion->instruction->op.nbr_cycles;
     champion->pc = (champion->pc + champion->instruction->size) % MEM_SIZE;
+    champion_load_instruction(vm->mem, champion);
     return (status);
 }
