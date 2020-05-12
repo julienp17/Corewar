74f07818a92eb5fc3a6de702231ef774773d4055
Julien Pause <julien.pause@epitech.eu>
2020-05-13 00:52:52 +0400

    Fix zjmp opeartion

--- corewar/src/operations/zjmp.c
+++ corewar/src/operations/zjmp.c
@@ -10,11 +10,17 @@
 int zjmp(vm_t *vm, champion_t *champion)
 {
     int index = 0;
+    int value = 0;
 
     (void)vm;
     if (champion->carry == false)
         return (EXIT_FAILURE);
-    index = champion->instruction->args[0].value;
+    value = champion->instruction->args[0].value;
+    if (value > 0)
+        index = (champion->pc + value % IDX_MOD) % MEM_SIZE;
+    else
+        index = (champion->pc + value) % MEM_SIZE;
     champion->pc = index;
+    champion->instruction->size = 0;
     return (EXIT_SUCCESS);
 }
