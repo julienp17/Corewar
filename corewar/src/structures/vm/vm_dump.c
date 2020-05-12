b6511492e4c27aab6253a0e6d63254f4feab5ae9
Julien Pause <julien.pause@epitech.eu>
2020-05-12 18:39:55 +0400

    Rename vm_dump_mem to vm_dump and fix it so its closer in output to reference corewar

--- corewar/src/structures/vm/vm_dump.c
+++ corewar/src/structures/vm/vm_dump.c
@@ -8,14 +8,14 @@
 #include <stdio.h>
 #include "vm.h"
 
-void vm_dump_mem(vm_t *vm)
+void vm_dump(vm_t *vm)
 {
     int const offset = 0x20;
 
     for (int i = 0 ; i < MEM_SIZE ; i += offset) {
         printf("%-4X : ", i);
         for (int j = 0 ; j < offset ; j++)
-            printf(" %02X", (unsigned char)vm->mem[i + j]);
+            printf("%02X ", (unsigned char)vm->mem[i + j]);
         printf("\n");
     }
 }
