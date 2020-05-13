5af995eaa4db5b878e4631ab23040430afda10ed
Julien Pause <julien.pause@epitech.eu>
2020-05-13 11:40:40 +0400

    Refactor sti

--- corewar/src/operations/sti.c
+++ corewar/src/operations/sti.c
@@ -11,6 +11,7 @@ int sti(vm_t *vm, champion_t *champion)
 {
     int offset_1 = 0;
     int offset_2 = 0;
+    int offset = 0;
     int value = 0;
     int address = 0;
     int reg_nb = 0;
@@ -19,8 +20,9 @@ int sti(vm_t *vm, champion_t *champion)
     value    = champion->regs[reg_nb - 1];
     offset_1 = champion->instruction->args[1].value;
     offset_2 = champion->instruction->args[2].value;
+    offset   = offset_1 + offset_2;
     for (unsigned long int i = 0 ; i < sizeof(int) ; i++) {
-        address = (champion->pc+ (offset_1 + offset_2 + i) % IDX_MOD)% MEM_SIZE;
+        address = (champion->pc + (offset + i) % IDX_MOD) % MEM_SIZE;
         vm->mem[address] = (unsigned char)(value >> (24 - (i * 8)));
     }
     return (EXIT_SUCCESS);
