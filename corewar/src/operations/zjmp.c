1085ad8d64fcda05cf0738973bf4c4c25eb93dc5
Julien Pause <julien.pause@epitech.eu>
2020-05-13 16:11:11 +0400

    Fix zjmp to work with a circular memory

--- corewar/src/operations/zjmp.c
+++ corewar/src/operations/zjmp.c
@@ -9,7 +9,6 @@
 
 int zjmp(vm_t *vm, champion_t *champion)
 {
-    int index = 0;
     int value = 0;
 
     (void)vm;
@@ -17,10 +16,13 @@ int zjmp(vm_t *vm, champion_t *champion)
         return (EXIT_FAILURE);
     value = champion->instruction->args[0].value;
     if (value > 0)
-        index = (champion->pc + value % IDX_MOD) % MEM_SIZE;
+        champion->pc += value % IDX_MOD;
     else
-        index = (champion->pc + value) % MEM_SIZE;
-    champion->pc = index;
+        champion->pc += value;
+    if (champion->pc < 0)
+        champion->pc += MEM_SIZE;
+    else if (champion->pc > MEM_SIZE)
+        champion->pc %= MEM_SIZE;
     champion->instruction->size = 0;
     return (EXIT_SUCCESS);
 }
