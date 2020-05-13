d6e7cfc33ea7196d985b1f746a3116da2a646b27
Julien Pause <julien.pause@epitech.eu>
2020-05-13 16:52:35 +0400

    Add get_index utility function

---
+++ corewar/src/utilities/get_index.c
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2020
+** Corewar VM
+** File description:
+** Returns an index that is certainly in the bounds of the vm
+*/
+
+#include "op.h"
+
+int get_index(int const pc, int const value)
+{
+    int index = 0;
+
+    if (value > 0)
+        index = pc + value % IDX_MOD;
+    else
+        index = pc + value;
+    if (index < 0)
+        index += MEM_SIZE;
+    else if (index > MEM_SIZE)
+        index %= MEM_SIZE;
+    return (index);
+}
