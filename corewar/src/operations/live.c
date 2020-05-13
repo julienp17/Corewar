98b34e93d1cf41d193a8d171f306c7a407a9d740
Julien Pause <julien.pause@epitech.eu>
2020-05-13 16:13:58 +0400

    Refactor live operation with regards to vm struct changes

--- corewar/src/operations/live.c
+++ corewar/src/operations/live.c
@@ -14,13 +14,18 @@ int live(vm_t *vm, champion_t *champion)
     champion_t *champion_living = NULL;
 
     player_nb = champion->instruction->args[0].value;
-    for (int i = 0 ; champion_living == NULL && i < MAX_CHAMPIONS ; i++)
+    for (int i = 0 ; champion_living == NULL && i < vm->nb_champions ; i++)
         if (player_nb == vm->champions[i].nb)
             champion_living = &(vm->champions[i]);
-    if (champion_living) {
-        champion_living->last_live = vm->cycle;
-        printf("The player %d (%s) is alive.\n", champion_living->nb,
-                                            champion_living->header.prog_name);
+    if (champion_living == NULL)
+        return (EXIT_FAILURE);
+    vm->nb_live++;
+    if (vm->nb_live >= NBR_LIVE) {
+        vm->cycle_to_die -= CYCLE_DELTA;
+        vm->nb_live = 0;
     }
+    champion_living->last_live = vm->cycle;
+    printf("The player %d (%s) is alive.\n", champion_living->nb,
+                                        champion_living->header.prog_name);
     return (EXIT_SUCCESS);
 }
