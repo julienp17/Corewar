e6ae6097c7face1a5de4082fbf75b8bacb776dbb
Julien Pause <julien.pause@epitech.eu>
2020-05-14 20:14:29 +0400

    Call get_index in sti instead of homemade address building

--- corewar/src/operations/sti.c
+++ corewar/src/operations/sti.c
@@ -6,7 +6,7 @@
 */
 
 #include "vm.h"
-
+#include <stdio.h>
 int sti(vm_t *vm, champion_t *champion)
 {
     int offset_1 = 0;
@@ -22,7 +22,7 @@ int sti(vm_t *vm, champion_t *champion)
     offset_2 = champion->instruction->args[2].value;
     offset   = offset_1 + offset_2;
     for (unsigned long int i = 0 ; i < sizeof(int) ; i++) {
-        address = (champion->pc + (offset + i) % IDX_MOD) % MEM_SIZE;
+        address = get_index(champion->pc, offset + i);
         vm->mem[address] = (unsigned char)(value >> (24 - (i * 8)));
     }
     return (EXIT_SUCCESS);
