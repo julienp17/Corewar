8a80c7dfb93452f2c573c0c0bf51f1e1ee7ee65d
Julien Pause <julien.pause@epitech.eu>
2020-05-14 21:14:34 +0400

    Fix sti to treat value as int if its below 128 when % to 256, cast to char otherwise

--- corewar/src/operations/sti.c
+++ corewar/src/operations/sti.c
@@ -6,7 +6,7 @@
 */
 
 #include "vm.h"
-#include <stdio.h>
+
 int sti(vm_t *vm, champion_t *champion)
 {
     int offset_1 = 0;
@@ -18,12 +18,14 @@ int sti(vm_t *vm, champion_t *champion)
 
     reg_nb   = champion->instruction->args[0].value;
     value    = champion->regs[reg_nb - 1];
+    if (value % 256 > 127)
+        value  = (char)value;
     offset_1 = champion->instruction->args[1].value;
     offset_2 = champion->instruction->args[2].value;
     offset   = offset_1 + offset_2;
     for (unsigned long int i = 0 ; i < sizeof(int) ; i++) {
         address = get_index(champion->pc, offset + i);
-        vm->mem[address] = (unsigned char)(value >> (24 - (i * 8)));
+        vm->mem[address] = (value >> (8 * (3 - i))) & 0xFF;
     }
     return (EXIT_SUCCESS);
 }
