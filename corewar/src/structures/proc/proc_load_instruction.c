4b257258005b60465a436991d0a6d405fffe4c94
Julien Pause <julien.pause@epitech.eu>
2020-05-14 22:22:39 +0400

    Add struct proc, add it as a field of champion structure, and rename champion_load_instruction to proc_load_instruction

--- corewar/src/structures/proc/proc_load_instruction.c
+++ corewar/src/structures/proc/proc_load_instruction.c
@@ -2,47 +2,47 @@
 ** EPITECH PROJECT, 2020
 ** Corewar VM
 ** File description:
-** Load the next command of a champion
+** Load the next instruction of a process
 */
 
 #include "vm.h"
 #include "corewar.h"
 
-static int load_instruction(char mem[MEM_SIZE], champion_t *champion);
+static int load_instruction(char mem[MEM_SIZE], proc_t *proc);
 static void fill_args_meta(instruction_t *instruction, uchar coding_byte);
-static void fill_arg_value(char mem[MEM_SIZE], champion_t *champion,arg_t *arg);
+static void fill_arg_value(char mem[MEM_SIZE], proc_t *proc, arg_t *arg);
 
-int champion_load_instruction(char mem[MEM_SIZE], champion_t *champion)
+int proc_load_instruction(char mem[MEM_SIZE], proc_t *proc)
 {
     int status = 0;
 
-    champion->pc = champion->pc + champion->instruction->size;
-    instruction_reset(champion->instruction);
-    status = load_instruction(mem, champion);
+    proc->pc += proc->instruction->size;
+    instruction_reset(proc->instruction);
+    status = load_instruction(mem, proc);
     if (status == EXIT_SUCCESS)
-        champion->cycle_wait = champion->instruction->op.nbr_cycles;
+        proc->cycle_wait = proc->instruction->op.nbr_cycles;
     else
-        champion->cycle_wait = 2;
+        proc->cycle_wait = 2;
     return (status);
 }
 
-static int load_instruction(char mem[MEM_SIZE], champion_t *champion)
+static int load_instruction(char mem[MEM_SIZE], proc_t *proc)
 {
     instruction_t *instruction = NULL;
     uchar coding_byte = 0;
 
-    instruction = champion->instruction;
-    instruction->op = op_get_by_code(mem[champion->pc]);
+    instruction = proc->instruction;
+    instruction->op = op_get_by_code(mem[proc->pc]);
     if (op_is_null(instruction->op))
         return (EXIT_FAILURE);
     instruction->size++;
     if (op_has_coding_byte(instruction->op))
-        coding_byte = mem[champion->pc + instruction->size++];
+        coding_byte = mem[proc->pc + instruction->size++];
     else
         coding_byte = NO_CB_MASK;
     fill_args_meta(instruction, coding_byte);
     for (int i = 0 ; i < instruction->op.nbr_args ; i++)
-        fill_arg_value(mem, champion, &(instruction->args[i]));
+        fill_arg_value(mem, proc, &(instruction->args[i]));
     if (arguments_are_correct(instruction->op, instruction->args) == false) {
         instruction->op = (op_t) {0, 0, {0}, 0, 0, 0};
         return (EXIT_FAILURE);
@@ -64,23 +64,23 @@ static void fill_args_meta(instruction_t *instruction, uchar coding_byte)
     }
 }
 
-static void fill_arg_value(char mem[MEM_SIZE], champion_t *champion, arg_t *arg)
+static void fill_arg_value(char mem[MEM_SIZE], proc_t *proc, arg_t *arg)
 {
     char buf[sizeof(int)] = {0, 0, 0, 0};
     int address = 0;
 
     for (int j = 0 ; j < arg->size ; j++) {
-        address = (champion->pc + champion->instruction->size++) % MEM_SIZE;
+        address = (proc->pc + proc->instruction->size++) % MEM_SIZE;
         buf[j] = mem[address];
     }
     if (arg->type == T_REG)
         arg->value = buf[0];
     if (arg->type == T_IND) {
         arg->value = swap_int16(*(short int *)(buf));
-        arg->value = (champion->pc + arg->value % IDX_MOD) % MEM_SIZE;
+        arg->value = (proc->pc + arg->value % IDX_MOD) % MEM_SIZE;
     }
     if (arg->type == T_DIR) {
-        if (argument_is_index(champion->instruction->op))
+        if (argument_is_index(proc->instruction->op))
             arg->value = swap_int16(*(short int *)(buf));
         else
             arg->value = swap_int32(*(int *)buf);
