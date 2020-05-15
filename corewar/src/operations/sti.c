ceb6657355d56236c85cbe0779b93c476fa7bb02
Julien Pause <julien.pause@epitech.eu>
2020-05-15 23:28:37 +0400

    Refactor sti operation

--- corewar/src/operations/sti.c
+++ corewar/src/operations/sti.c
@@ -7,50 +7,46 @@
 
 #include "vm.h"
 
-static int get_total_offset(arg_t args[MAX_ARGS_NUMBER], int regs[REG_NUMBER]);
-static int get_offset(arg_t *arg, int regs[REG_NUMBER]);
+static int get_total_offset(char mem[MEM_SIZE], proc_t *proc);
+static int get_offset(char mem[MEM_SIZE], proc_t *proc, arg_t *arg);
 
 int sti(vm_t *vm, proc_t *proc)
 {
     int offset = 0;
-    int value = 0;
-    int address = 0;
+    char to_store = 0;
     int reg_nb = 0;
 
     reg_nb = proc->instruction->args[0].value;
-    value  = proc->regs[reg_nb - 1];
-    if (value % 256 > 127)
-        value  = (char)value;
-    offset = get_total_offset(proc->instruction->args, proc->regs);
-    for (unsigned long int i = 0 ; i < sizeof(int) ; i++) {
-        address = get_index(proc->pc, offset + i, IDX_MOD);
-        vm->mem[address] = (value >> (8 * (3 - i))) & 0xFF;
-    }
+    to_store = proc->regs[reg_nb - 1];
+    offset = get_total_offset(vm->mem, proc);
+    set_mem_value(vm->mem, get_index(proc->pc, offset, IDX_MOD), to_store);
     return (EXIT_SUCCESS);
 }
 
-static int get_total_offset(arg_t args[MAX_ARGS_NUMBER], int regs[REG_NUMBER])
+static int get_total_offset(char mem[MEM_SIZE], proc_t *proc)
 {
     int offset = 0;
     int offset_1 = 0;
     int offset_2 = 0;
 
-    offset_1 = get_offset(&(args[1]), regs);
-    offset_2 = get_offset(&(args[2]), regs);
+    offset_1 = get_offset(mem, proc, &(proc->instruction->args[1]));
+    offset_2 = get_offset(mem, proc, &(proc->instruction->args[2]));
     offset = offset_1 + offset_2;
     return (offset);
 }
 
-static int get_offset(arg_t *arg, int regs[REG_NUMBER])
+static int get_offset(char mem[MEM_SIZE], proc_t *proc, arg_t *arg)
 {
     int reg_nb = 0;
     int offset = 0;
 
     if (arg->type == T_REG) {
         reg_nb = arg->value;
-        offset = regs[reg_nb - 1];
-    } else {
+        offset = proc->regs[reg_nb - 1];
+    } else if (arg->type == T_DIR) {
         offset = arg->value;
+    } else {
+        offset = get_mem_value(mem, get_index(proc->pc, arg->value, IDX_MOD));
     }
     return (offset);
 }
