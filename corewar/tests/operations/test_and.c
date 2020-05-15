d8a072b7f9f976eda91c8ed7846e1fb2df859744
tristan bourgeois <tristan@laptop-q0eotgt3-1.home>
2020-05-15 15:07:31 +0400

    unit test for: and, or, xor operations

---
+++ corewar/tests/operations/test_and.c
@@ -0,0 +1,73 @@
+/*
+** EPITECH PROJECT, 2020
+** corewar
+** File description:
+** test_and
+*/
+
+#include <criterion/criterion.h>
+#include "operation.h"
+
+Test(and, simple_and_operation)
+{
+    int status = 0;
+    int expected = 4;
+    proc_t *process = proc_create(0);
+
+    process->instruction->args[0].value = 2;
+    process->instruction->args[1].value = 3;
+    process->instruction->args[2].value = 4;
+    process->regs[1] = 5;
+    process->regs[2] = 4;
+    status = and(NULL, process);
+    cr_assert_eq(status, expected);
+    cr_assert_eq(process->regs[3], expected);
+}
+
+Test(and, and_with_two_negative)
+{
+    int status = 0;
+    int expected = -8;
+    proc_t *process = proc_create(0);
+
+    process->instruction->args[0].value = 2;
+    process->instruction->args[1].value = 3;
+    process->instruction->args[2].value = 4;
+    process->regs[1] = -2;
+    process->regs[2] = -7;
+    status = and(NULL, process);
+    cr_assert_eq(status, expected);
+    cr_assert_eq(process->regs[3], expected);
+}
+
+Test(and, and_with_one_negative)
+{
+    int status = 0;
+    int expected = -30;
+    proc_t *process = proc_create(0);
+
+    process->instruction->args[0].value = 2;
+    process->instruction->args[1].value = 3;
+    process->instruction->args[2].value = 4;
+    process->regs[1] = -5;
+    process->regs[2] = -30;
+    status = and(NULL, process);
+    cr_assert_eq(status, expected);
+    cr_assert_eq(process->regs[3], expected);
+}
+
+Test(and, and_with_big_numbers)
+{
+    int status = 0;
+    int expected = 4786;
+    proc_t *process = proc_create(0);
+
+    process->instruction->args[0].value = 2;
+    process->instruction->args[1].value = 3;
+    process->instruction->args[2].value = 4;
+    process->regs[1] = 23231;
+    process->regs[2] = 5874;
+    status = and(NULL, process);
+    cr_assert_eq(status, expected);
+    cr_assert_eq(process->regs[3], expected);
+}
---
+++ corewar/tests/operations/test_or.c
@@ -0,0 +1,73 @@
+/*
+** EPITECH PROJECT, 2020
+** corewar
+** File description:
+** test_or
+*/
+
+#include <criterion/criterion.h>
+#include "operation.h"
+
+Test(or, simple_or_operation)
+{
+    int status = 0;
+    int expected = 13;
+    proc_t *process = proc_create(0);
+
+    process->instruction->args[0].value = 2;
+    process->instruction->args[1].value = 3;
+    process->instruction->args[2].value = 4;
+    process->regs[1] = 5;
+    process->regs[2] = 8;
+    status = or(NULL, process);
+    cr_assert_eq(status, expected);
+    cr_assert_eq(process->regs[3], expected);
+}
+
+Test(or, or_with_two_negative)
+{
+    int status = 0;
+    int expected = -5;
+    proc_t *process = proc_create(0);
+
+    process->instruction->args[0].value = 2;
+    process->instruction->args[1].value = 3;
+    process->instruction->args[2].value = 4;
+    process->regs[1] = -21;
+    process->regs[2] = -38;
+    status = or(NULL, process);
+    cr_assert_eq(status, expected);
+    cr_assert_eq(process->regs[3], expected);
+}
+
+Test(or, or_with_one_negative)
+{
+    int status = 0;
+    int expected = -90;
+    proc_t *process = proc_create(0);
+
+    process->instruction->args[0].value = 2;
+    process->instruction->args[1].value = 3;
+    process->instruction->args[2].value = 4;
+    process->regs[1] = 6;
+    process->regs[2] = -94;
+    status = or(NULL, process);
+    cr_assert_eq(status, expected);
+    cr_assert_eq(process->regs[3], expected);
+}
+
+Test(or, or_with_big_numbers)
+{
+    int status = 0;
+    int expected = 1007263;
+    proc_t *process = proc_create(0);
+
+    process->instruction->args[0].value = 2;
+    process->instruction->args[1].value = 3;
+    process->instruction->args[2].value = 4;
+    process->regs[1] = 859674;
+    process->regs[2] = 154263;
+    status = or(NULL, process);
+    cr_assert_eq(status, expected);
+    cr_assert_eq(process->regs[3], expected);
+}
