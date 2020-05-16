38bc992d754b9e77701dc0aa9dc0901fd62a47f2
Dorian BEASSE <dorian.beasse@epitech.eu>
2020-05-16 17:30:52 +0400

    Add player

--- champions/src/thebest.s
+++ champions/src/thebest.s
@@ -1,4 +1,20 @@
-.name "The Best"
-.comment "Dorian 0 commit"
+.name "Julien Lepers"
+.comment "Ouai, Ouai Ouai, Ouai"
 
-sti r1, %7, %1
+jump: sti r1, %7, %1
+live %1
+fork %100
+zjmp %:init
+
+init: sti r1, %:live,%1
+ld %355,r2
+ld %4,r8
+
+live: live %1
+add r2,r3,r2
+sti r6, %:live,r2
+ldi r7,r8,r6
+add r7,r8,r7
+and r4, %0, r4
+
+zjmp %:live
