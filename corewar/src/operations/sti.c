d944d1312152f961b2664c794a1c8c59d830a567
Julien Pause <julien.pause@epitech.eu>
2020-05-16 14:42:39 +0400

    Refactor sti operation with call to argument_get_value

--- corewar/src/operations/sti.c
+++ corewar/src/operations/sti.c
@@ -8,7 +8,6 @@
 #include "vm.h"
 
 static int get_total_offset(char mem[MEM_SIZE], proc_t *proc);
-static int get_offset(char mem[MEM_SIZE], proc_t *proc, arg_t *arg);
 
 int sti(vm_t *vm, proc_t *proc)
 {
@@ -29,24 +28,8 @@ static int get_total_offset(char mem[MEM_SIZE], proc_t *proc)
     int offset_1 = 0;
     int offset_2 = 0;
 
-    offset_1 = get_offset(mem, proc, &(proc->instruction->args[1]));
-    offset_2 = get_offset(mem, proc, &(proc->instruction->args[2]));
+    offset_1 = argument_get_value(mem, proc, 1, IDX_MOD);
+    offset_2 = argument_get_value(mem, proc, 2, 0);
     offset = offset_1 + offset_2;
-    return (offset);
-}
-
-static int get_offset(char mem[MEM_SIZE], proc_t *proc, arg_t *arg)
-{
-    int reg_nb = 0;
-    int offset = 0;
-
-    if (arg->type == T_REG) {
-        reg_nb = arg->value;
-        offset = proc->regs[reg_nb - 1];
-    } else if (arg->type == T_DIR) {
-        offset = arg->value;
-    } else {
-        offset = get_mem_value(mem, get_index(proc->pc, arg->value, IDX_MOD));
-    }
     return (offset);
 }
